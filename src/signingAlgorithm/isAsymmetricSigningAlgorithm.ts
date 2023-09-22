56b863f13a086c8a2f2035bf2805041773d9f88b
Ulad Kasach <uladkasach@gmail.com>
2023-09-22 09:36:32 -0400

    fix(alg): identify ES256, ES384, and ES512 as known asymetric algs

--- src/signingAlgorithm/isAsymmetricSigningAlgorithm.ts
+++ src/signingAlgorithm/isAsymmetricSigningAlgorithm.ts
@@ -1,4 +1,11 @@
-const ASYMMETRIC_SIGNING_ALGORITHMS = ['RS256', 'RS384', 'RS512'] as const;
+const ASYMMETRIC_SIGNING_ALGORITHMS = [
+  'RS256',
+  'RS384',
+  'RS512',
+  'ES256',
+  'ES384',
+  'ES512',
+] as const;
 export type AsymmetricSigningAlgorithm =
   typeof ASYMMETRIC_SIGNING_ALGORITHMS[number];
 export const isAsymmetricSigningAlgorithm = (
